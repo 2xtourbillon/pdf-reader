105c209d7a8832ff0006b17883065ef8e34b2218
Gurgen Verdian <gurgen.verdian@gmail.com>
2022-02-13 11:02:51 -0500

    added labels/buttons

--- pdf_audio_reader.py
+++ pdf_audio_reader.py
@@ -1,7 +1,20 @@
 from tkinter import *
 from tkinter import filedialog
+from tracemalloc import stop
 import PyPDF2
 import pyttsx3
+
+def extract_text():
+    """Opening and extract a pdf file"""
+    file = filedialog.askopenfile(parent=root, mode='rb', title='Choose a PDF File')
+    if file != None:
+        pdfReader = PyPDF2.PdfFileReader(file)
+        global text_extracted
+        text_extracted = ''
+        for pageNum in range(pdfReader.numPages):
+            pageObject = pdfReader.getPage(pageNum)
+            text_extracted += pageObject.extractText()
+        file.close()
 
 def Application(root):
     
@@ -15,7 +28,6 @@ def Application(root):
     frame2 = Frame(root, width=500, height=450, bg='light grey')
     frame1.pack(side='top', fill='both')
     frame2.pack(side='top', fill='y')
-    
     
 
     #frame1 widgets
@@ -31,16 +43,37 @@ def Application(root):
     
     rate_text = Label(frame2, text='Enter Rate of Speech', fg='black', bg='aqua',\
         font=('Arial 12'))
-    rate_text.grid(row=1, column=0, pady=15, padx=0, sticky=W)
+    rate_text.grid(row=1, pady=15, padx=0, sticky=W)
 
     rate = Entry(frame2, text='200', fg='black', bg='white', font=('Arial 12'))
     rate.grid(row=1, column=1, padx=30, pady=15, sticky=W)
 
+    voice_text = Label(frame2, text='Select Voice:', fg='black', bg='aqua', font=('Arial 12'))
+    voice_text.grid(row=2, column=0, pady=15, padx=0, sticky=E)
 
-    mainloop()
+    male = IntVar()
+    maleOpt = Checkbutton(frame2, text='Male', bg='pink', variable=male, onvalue=1, offvalue=0)
+    maleOpt.grid(row=2, column=1, pady=0, padx=30, sticky=W)
+
+    female = IntVar()
+    femaleOpt = Checkbutton(frame2, text='Female', bg='pink', variable=female, onvalue=1, offvalue=0)
+    femaleOpt.grid(row=3, column=1, pady=0, padx=30, sticky=W)
+
+    submitBtn = Button(frame2, text='Play PDF File', activeforeground='red', \
+        padx=60, pady=10, fg='white', bg='black', font=('Arial 12')) #command=speak_text)
+    submitBtn.grid(row=4, column=0, pady=65)
+
+    stopBtn = Button(frame2, text='Stop Playing', activeforeground='red', \
+        padx=60, pady=10, fg='white', bg='black', font=('Arial 12')) # command=stop_speaking
+    stopBtn.grid(row=4, column=1, pady=65)
+
 
 
 if __name__ == '__main__':
-    window = Tk()
-    Application(window)
+    mytext, rate, male, female = '', 100, 0, 0
+    engine = pyttsx3.init()
+    
+    root = Tk()
+    Application(root)
+    root.mainloop()
     
